162ea9dac94650edaa860a16546a5fd657f65390
John Bellone <jb@thunkbrightly.com>
2011-08-30 07:04:09 -0400

    Processor support for generator

---
+++ lib/blaze/engines/codeigniter/codeigniter_generate.php
@@ -0,0 +1,8 @@
+<?php if (! defined('BLAZE_PATH')) exit("No direct script access allowed");
+
+require_once BLAZE_PATH . "blaze_processor.php"
+
+class Codeigniter_generate extends Blaze_Processor
+{
+
+}
