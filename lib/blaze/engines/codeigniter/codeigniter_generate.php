ee2be165259915cbcefe39f20779bb3889bd1898
John Bellone <jb@thunkbrightly.com>
2011-09-03 01:12:44 -0400

    Template support and controller generation

--- lib/blaze/engines/codeigniter/codeigniter_generate.php
+++ lib/blaze/engines/codeigniter/codeigniter_generate.php
@@ -11,6 +11,28 @@ require_once BLAZE_PATH . "blaze_processor.php";
  */
 class Codeigniter_generate extends Blaze_Processor
 {
+    protected function construct_method_string($tmpl_dir, $arguments)
+    {
+        $output = "";
+        
+        if (count($arguments) >= 0)
+        {
+            $method_tmpl = file_get_contents($tmpl_dir . "method.tmpl");                             
+            $reg = "/^([a-zA-Z0-9])+/";
+        
+            while (($arg = array_shift($arguments)) !== null)
+            {
+                if (preg_match($reg, $arg))
+                {
+                    $name = strtolower($arg);
+                    $output .= str_replace("%METHOD_NAME%", $name, $method_tmpl);
+                }                
+            }
+        }
+
+        return $output;
+    }
+    
     public function __construct()
     {
 
@@ -25,7 +47,26 @@ class Codeigniter_generate extends Blaze_Processor
         }
 
         // Minimum required is just the class name to generate the file.
-        $klass = array_shift($arguments);
+        $class_tolower = strtolower(array_shift($arguments));
+        $class_ucfirst = ucfirst($class_tolower);
+        
+        // Load up the templates.
+        $tmpl_dir = dirname(__FILE__) . "/templates/";
+        $class_tmpl = file_get_contents($tmpl_dir . "controller.tmpl");
+
+        // Parse array for methods and load their template.
+        $method_output = $this->construct_method_string($tmpl_dir, $arguments);
+
+        $inputs = array("%CLASS_UCFIRST%", "%METHOD_OUTPUT%", "%CLASS_TOLOWER%");
+        $outputs = array($class_ucfirst, $method_output, $class_tolower);
+        $output = str_replace($inputs, $outputs, $class_tmpl);
+
+        $filename = $class_tolower . ".php";
+        $output_path = CODEIGNITER_PATH . "/application/controllers/" . $filename;
+
+        file_put_contents($output_path, $output);
+        
+        return true;
     } 
     
     public function help()
